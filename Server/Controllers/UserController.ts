3c13f251acea0f354c4d276801c4f68c855db5f8
Wilman Martinez <wilman.martinez@onerf.com.br>
2023-09-02 15:32:59 -0300

    Flipped user validation to controller.

--- Server/Controllers/UserController.ts
+++ Server/Controllers/UserController.ts
@@ -1,44 +1,79 @@
+import express from "express";
+import { ResponseHandler } from "../Helpers/ResponseHandler";
 import { UserCollection } from "../Models/User";
-
+import { IUser } from "../Interfaces/User";
+import { ObjHandler } from "../Helpers/ObjectHandler";
+const objHandler: ObjHandler = new ObjHandler();
+const responseHandler: ResponseHandler = new ResponseHandler();
 export class UserController {
     private local: string = "[USER-CONTROLLER]";
 
-    public async CreateUser(userData: object): Promise<object> {
+    public async CreateUser(userData: IUser, response: express.Response): Promise<object> {
         try {
-            return await UserCollection.create(userData);
+            
+            const userDataIsObject: boolean = objHandler.IsObjectNotEmpty(userData);
+            if (userDataIsObject == false) {
+                return responseHandler.ReturnResponseMessage("Error", "User data is not a valid object.", 400, response);
+            }
+
+            const result: object = await UserCollection.create(userData);
+            return responseHandler.ReturnResponseMessage("Success", "User created.", 200, response, result);
         } catch(error) {
-            console.error(`${this.local} Failed trying to create a new user: ${error}`);
             throw error;
         }
     }
 
-    public async DeleteUser(userID: string): Promise<object> {
+    public async DeleteUser(userID: string, response: express.Response): Promise<object> {
         try {
             if(!userID) {
-                console.error(`${this.local} User id not defined.`);
-                throw new Error("User id not defined.");
+                return responseHandler.ReturnResponseMessage("Error", "User id not defined", 400, response);
             }
-            return await UserCollection.findByIdAndDelete(userID);
+            const result: object | undefined | null = await UserCollection.findByIdAndDelete(userID);
+
+            if (result == undefined || result == null) {
+                return responseHandler.ReturnResponseMessage("Info", "User id not founded", 404, response);
+            }
+
+            const isResultValid: boolean = objHandler.IsObjectNotEmpty(result);
+            if (isResultValid == false) {
+                return responseHandler.ReturnResponseMessage("Error", "Failed trying to get user infos.", 400, response);
+            }
+            return responseHandler.ReturnResponseMessage("Success", "User deleted.", 200, response, result);
         } catch (error) {
-            console.error(`${this.local} Failed trying to delete user by id: ${error}`);
             throw error;
         }
     }
 
-    public async GetUsers(): Promise<Object> {
+    public async GetUsers(response: express.Response): Promise<object> {
         try {
-            return await UserCollection.find({});
+            const result: object[] = await UserCollection.find({});
+            const isResultArray: boolean = objHandler.IsArrayNotEmpty(result);
+            if (isResultArray == false) {
+                return responseHandler.ReturnResponseMessage("Info", "Users not founded.", 404, response);
+            }
+            return responseHandler.ReturnResponseMessage("Success", "Users founded.", 200, response, result);
         } catch (error) {
-            console.error(`${this.local} Failed trying to get users: ${error}`);
             throw error;
         }
     }
 
-    public async GetUserByID(userID: string): Promise<object> {
+    public async GetUserByID(userID: string, response: express.Response): Promise<object> {
         try {
-            return await UserCollection.findById(userID);
+            if (!userID) {
+                return responseHandler.ReturnResponseMessage("Error", "User id not defined.", 400, response);
+            }
+
+            const result: object | null | undefined = await UserCollection.findById(userID)
+            if (result == null || result == undefined) {
+                return responseHandler.ReturnResponseMessage("Info", "User not founded.", 404, response);
+            }
+
+            const isResultValid: boolean = objHandler.IsObjectNotEmpty(result);
+            if (isResultValid == false) {
+                return responseHandler.ReturnResponseMessage("Error", "Failed trying to get user infos.", 400, response);
+            }
+            return responseHandler.ReturnResponseMessage("Success", "User founded.", 200, response, result);
         } catch (error) {
-            console.error(`${this.local} Failed trying to find user.`);
             throw error;
         }
     }
--- Server/Routes/RoutesAPI/RoutesAPI.ts
+++ Server/Routes/RoutesAPI/RoutesAPI.ts
@@ -13,42 +13,25 @@ export class RoutesAPI {
     private DefineAPIs(): void {
         this.router.post("/api/users/create", async (req: express.Request, res: express.Response) => {
             try {
-                const validateBodyRequest: boolean = objHandler.IsObjectNotEmpty(req.body);
-                if (validateBodyRequest == false) {
-                    responseHandler.ReturnResponseMessage("Error", "Body request not defined", 404, res);
-                } else {
-                    await userController.CreateUser(req.body);
-                    responseHandler.ReturnResponseMessage("Success", "User created.", 200, res);
-                }
-            } catch(error) {
+                await userController.CreateUser(req.body, res);
+            } catch (error) {
                 console.error(`${this.local} Internal server error: ${error}`);
-                responseHandler.ReturnResponseMessage("Error", `Internal server error: ${error.message}`, 500, res);
+                responseHandler.ReturnResponseMessage("Error", `${error.message}`, 500, res);
             }
         });
 
         this.router.delete("/api/users/deleteUser/:id", async (req: express.Request, res: express.Response) => {
             try {
-                const userID: string = req.params.id;
-                if (!userID) {
-                    console.error(`${this.local} User id not defined.`);
-                    responseHandler.ReturnResponseMessage("Error", "User id not defined.", 404, res);
-                } else {
-                    responseHandler.ReturnResponseMessage("Sucess", `User with id ${userID} deleted.`, 200, res);
-                }
+                await userController.DeleteUser(req.params.id, res);
             } catch(error) {
-                responseHandler.ReturnResponseMessage("Error", `Internal server error: ${error.message}`, 500, res);
+                console.error(`${this.local} Internal server error: ${error}`);
+                responseHandler.ReturnResponseMessage("Error", `${error.message}`, 500, res);
             }
         });
 
         this.router.get("/api/users/getUsers", async (req: express.Request, res: express.Response) => {
             try {
-                const result: object = await userController.GetUsers();
-                const IsArrayNotEmpty: boolean = objHandler.IsArrayNotEmpty(result);
-                if (IsArrayNotEmpty == false) {
-                    responseHandler.ReturnResponseMessage("Error", "Users not founded.", 404, res);
-                } else {
-                    responseHandler.ReturnResponseMessage("Succes", "Users founded", 200, res, result);
-                }
+                await userController.GetUsers(res);
             } catch(error) {
                 console.error(`${this.local} Internal server error: ${error}`);
                 responseHandler.ReturnResponseMessage("Error", `Internal server error: ${error.message}`, 500, res);
@@ -57,19 +40,7 @@ export class RoutesAPI {
 
         this.router.get("/api/users/getUser/:id", async (req: express.Request, res: express.Response) => {
             try {
-                const userID: string = req.params.id;
-                if (!userID) {
-                    console.info(`${this.local} User id not defined.`);
-                    responseHandler.ReturnResponseMessage("Info", "User id not defined", 404, res);
-                    return;
-                }
-                const result: object = await userController.GetUserByID(userID);
-                const isObjectNotEmpty: boolean = objHandler.IsObjectNotEmpty(result);
-                if (isObjectNotEmpty == false) {
-                    responseHandler.ReturnResponseMessage("Error", "User not founded.", 404, res);
-                } else {
-                    responseHandler.ReturnResponseMessage("Success", "User founded", 200, res, result);
-                }
+                await userController.GetUserByID(req.params.id, res);
             } catch (error) {
                 console.error(`${this.local} Internal server error: ${error}`);
                 responseHandler.ReturnResponseMessage("Error", `Internal server error: ${error.message}`, 500, res);
